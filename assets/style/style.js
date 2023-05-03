18be6fc4dc7524df4dfaa7bf71228cdb0d02ccb5
Muhammad Faiz Al Azmi <muhammadfaizalazmi@gmail.com>
2023-05-03 10:10:02 +0700

    second commit

---
+++ assets/style/style.js
@@ -0,0 +1,22 @@
+var about_me = document.querySelector('#about-me');
+about_me.innerHTML = "Hello! My Name is Muhammad Faiz Al Azmi, i'm from Banjarnegara, I studied at Institut of Technology Telkom purwokerto, majoring in Informatics.";
+
+function facebook() {
+	window.open('https://web.facebook.com/muhammad.f.azmi.5477/','_blank');
+}
+
+function instagram() {
+	window.open('https://www.instagram.com/muhammadfaiz2709/','_blank');
+}
+
+function github() {
+	window.open('https://github.com/Pokrek2709','_blank');
+}
+
+function linkedin() {
+	window.open('https://www.linkedin.com/in/muhammadfaizalazmi/','_blank');
+}
+
+function email() {
+	window.open('mailto:muhammadfaizalazmi@gmail.com','_blank');
+}
